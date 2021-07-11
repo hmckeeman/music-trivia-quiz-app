973be1578dde6470d6d3c10bb5394680448bcdc8
hmckeeman <85905340+hmckeeman@users.noreply.github.com>
2021-07-11 15:14:50 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,47 @@
+<style>
+	main {
+	  text-align: center;
+	  padding: 1em;
+	  max-width: 240px;
+	  margin: 0 auto;
+	}
+	@media (min-width: 640px) {
+	  main {
+		max-width: none;
+	  }
+	}
+
+	h2 {
+		color: #bdaad4;
+		font-size: 4em;
+		font-weight: 100;
+	}
+  </style>
+  
+  <script>
+	import QuizArea from './QuizArea.svelte';
+	let playerScore = 0;
+	let visible = true;
+  </script>
+  
+  <main>
+	<ul>
+		<h2>Music Trivia!</h2>
+	</ul>
+
+	{#if visible}
+	  <p style="color: white; font-size: 24px">
+		Score:
+		<i>{playerScore} / 10</i>
+	  </p>
+	{/if}
+  
+	<QuizArea
+	  on:score={e => {
+		playerScore = e.detail.score;
+	  }}
+	  on:resultsScreen={e => {
+		visible = e.detail.showScore;
+	  }} />
+  
+  </main>
